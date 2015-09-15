fc82f203619e3d5aeebf42374e3607c4cd0ccfbf
John Donovan <john.donovan@gtnexus.com>
2015-09-15 10:09:45 -0700

    Making companion composable

--- src/companion.js
+++ src/companion.js
@@ -1,31 +1,46 @@
 /* global require */
-(function(module, undefined){
-    "use strict";
-    var fs, vm, path, load;
+var companion = (function(module, undefined) {
+  "use strict";
+  var fs, vm, path, load, compose, explicitParent, getParent, instance;
 
-    fs = require('fs');
-    vm = require('vm');
-    path = require('path');
+  fs = require('fs');
+  vm = require('vm');
+  path = require('path');
 
-    load = function(filename, context, callback){
-        var poly = typeof context === 'function';
+  compose = (parent) => {
+    explicitParent = parent;
+    return instance;
+  };
 
-        filename = path.resolve(path.dirname(module.parent.filename), filename);
-        callback = poly ? context : callback;
-        context = poly ? {} : (context || {});
+  getParent = () => {
+    return explicitParent || module.parent.filename;
+  };
 
-        if (callback){
-            fs.readFile(filename, function(err, data){
-                if (!err) {
-                    vm.runInNewContext(data, context, filename);
-                }
-                callback(err, context);
-            });
-        } else {
-            vm.runInNewContext(fs.readFileSync(filename), context, filename);
+  load = (filename, context, callback) => {
+    var poly = typeof context === 'function';
+    filename = path.resolve(path.dirname(getParent()), filename);
+    callback = poly ? context : callback;
+    context = poly ? {} : (context || {});
+    if (callback) {
+      fs.readFile(filename, function(err, data) {
+        if (!err) {
+          vm.runInNewContext(data, context, filename);
         }
-        return context;
-    };
+        callback(err, context);
+      });
+    } else {
+      vm.runInNewContext(fs.readFileSync(filename), context, filename);
+    }
+    return context;
+  };
 
-    module.exports = { require : load };
-}(module));
+  instance = {
+    compose: compose,
+    require: load
+  };
+
+  return instance;
+
+}());
+
+module.exports = companion;

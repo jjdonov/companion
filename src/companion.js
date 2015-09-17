d5c3f5028572d739cbece83b302bf0048687433e
John Donovan <john.donovan@gtnexus.com>
2015-09-16 17:26:43 -0700

    ability to require whole dir

--- src/companion.js
+++ src/companion.js
@@ -1,26 +1,34 @@
 /* global require */
 var companion = (function(module, undefined) {
   "use strict";
-  var fs, vm, path, load, compose, explicitParent, getParent, instance;
+  var fs, vm, path, explicitParent, instance, walk;
 
   fs = require('fs');
   vm = require('vm');
   path = require('path');
+  walk = require('walk');
 
-  compose = (parent) => {
+  function compose(parent) {
     explicitParent = parent;
     return instance;
-  };
+  }
 
-  getParent = () => {
+  function getParent() {
     return explicitParent || module.parent.filename;
-  };
+  }
 
-  load = (filename, context, callback) => {
+  function load(name, context, callback) {
     var poly = typeof context === 'function';
-    filename = path.resolve(path.dirname(getParent()), filename);
+    name = path.resolve(path.dirname(getParent()), name);
     callback = poly ? context : callback;
     context = poly ? {} : (context || {});
+    if (fs.lstatSync(name).isDirectory()) {
+      return loadModule(name, context, callback);
+    }
+    return loadFile(name, context, callback);
+  }
+
+  function loadFile(filename, context, callback) {
     if (callback) {
       fs.readFile(filename, function(err, data) {
         if (!err) {
@@ -32,7 +40,41 @@ var companion = (function(module, undefined) {
       vm.runInNewContext(fs.readFileSync(filename), context, filename);
     }
     return context;
-  };
+  }
+
+  function loadModule(moduleName, context, callback) {
+    let scripts = [];
+    let options = {
+      followLinks: false,
+      filters: [
+        'PlatformLocalization',
+        'TypeExtensionD1',
+        'xsd'
+      ],
+      listeners: {
+        file: (root, fileStats, next) => {
+          if (fileStats.name.endsWith('.js')) {
+            let resolved = path.resolve(root, fileStats.name);
+            let s = fs.readFileSync(resolved);
+            scripts.push(s);
+          }
+          next();
+        },
+        errors: (root, nodeStatsArray, next) => {
+          //TODO
+          next();
+        }
+      },
+
+    };
+    walk.walkSync(moduleName, options);
+    if (!scripts.length) {
+      throw new Error('Did not find any scripts in ' + moduleName);
+    }
+    let uniScript = scripts.join('\n');
+    vm.runInNewContext(uniScript, context, moduleName);
+    return context;
+  }
 
   instance = {
     compose: compose,
